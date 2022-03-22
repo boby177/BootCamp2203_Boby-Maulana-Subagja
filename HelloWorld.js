ec2b6ca83059c0fb779155f47b50f2cfec4b66f6
boby177 <bomasu37@gmail.com>
2022-03-22 13:51:28 +0700

    Menambahkan input  daftar 3 pertanyaan

---
+++ HelloWorld.js
@@ -0,0 +1,25 @@
+// Menampilkan nama dalam sebuah variabel
+const nama = "Boby Maulana Subagja";
+const tampilNama = `Halo ${nama}, Terimakasih telah memilih penerbangan kami.`;
+console.log(tampilNama);
+console.log();
+
+// Membuat readline untuk proses input dan output
+const readline = require('readline');
+const pertanyaan = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+});
+
+// Daftar pertanyaan
+console.log('=====> Welcome to Bob Air Flight <=====');
+pertanyaan.question('Berapa banyak jumlah tiket yang dibeli: ', (tiket) => {
+    pertanyaan.question('Dimanakah kota anda saat ini: ', (alamat) => {
+        pertanyaan.question('Masukkan kota tujuan anda pergi: ', (tujuan) => {
+            console.log();
+            console.log(`Selamat datang ${nama}, jumlah tiket yang ada beli sebanyak ${tiket} tiket.`);
+            console.log(`Anda saat ini berada di ${alamat}, dan berangkat pergi menuju kota ${tujuan}, selamat jalan !!!!`);
+            pertanyaan.close();
+        });
+    });
+});
